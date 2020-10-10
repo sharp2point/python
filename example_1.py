70403ec0f9eda2b0a79e2dd7a367cc822a81ae41
sharp2point <56555752+sharp2point@users.noreply.github.com>
2020-10-10 18:57:38 +0300

    Update example_1.py

--- example_1.py
+++ example_1.py
@@ -4,5 +4,5 @@ lst = [1,1.78,True,"Hello world !"
         ,[1,2],(1,2),{1,2}
         ,{1:"Name",2:"Family"}]
 for i in enumerate(lst):
-    parse_el_type = str(type(i[1])).strip(">").split()
+    parse_el_type = str(type(i[1])).strip(">").split() 
     print(f"{i[0]}. Элемент: {i[1]} : имеет тип: {parse_el_type[1]}")
