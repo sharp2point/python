621ebc0eec4f40e8ca9335e234c6fe96728d0400
sharp2point <56555752+sharp2point@users.noreply.github.com>
2020-10-15 11:20:44 +0300

    Update example_1.py

--- example_1.py
+++ example_1.py
@@ -7,9 +7,13 @@ def carry_dividend(dividend_num: float):
 
     return carry_divider
 
-
 dividend_num = float(input("Введите делимое число:"))
 quotient_num = carry_dividend(dividend_num)
 
-divider_num = float(input("Введите число делитель:"))
+while True:
+    divider_num = float(input("Введите число делитель:"))
+    if divider_num == 0:
+        print("Ошибка ввода, деление на 0")
+    else:
+        break
 print(f"Результат деления {dividend_num}/{divider_num} = {quotient_num(divider_num):.2}")
